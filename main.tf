e5d7f29af6fc827d27e70956af361e0d1cf715b6
jadedgurl <126923594+jadedgurl@users.noreply.github.com>
2023-06-05 18:40:16 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+# Provider Block
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "ec2_example" {
+  ami           = "admin-terraform-aws"
+  instance_type =  "t2.micro"
+  tags = {
+    Name = "MCIT"
+  }
+}
+
+
+# This null_resource will be executed everytime because of id = time().
+resource "null_resource" "null_resource_simple" {
+  
+  # Look carefully in the trigger we have assigned time() which we change value every time you run $terraform apply command.
+  triggers = {
+    id = time()
+  }
+
+  provisioner "local-exec" {
+    command = "echo Hello World"
+  }
+} 
